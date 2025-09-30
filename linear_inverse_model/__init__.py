f188d1e123dad366bce483a03b5a04205a337f9c
Yuxin Wang <yuxinw@hawaii.edu>
2025-09-30 09:36:14 -1000

    Update __init__.py

--- linear_inverse_model/__init__.py
+++ linear_inverse_model/__init__.py
@@ -1,2 +1,8 @@
-from . import LIM_utils, STLIM, CSLIM
-__all__ = ["LIM_utils", "STLIM", "CSLIM"]
+"""linear_inverse_model (LIM) toolbox."""
+
+__all__ = ["CSLIM", "STLIM", "LIM_utils", "__version__"]
+__version__ = "0.1.0"
+
+from .CSLIM import CSLIM
+from .STLIM import STLIM
+from . import LIM_utils
